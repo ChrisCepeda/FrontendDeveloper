1997e6de058333487b091b85c5e46c0b2f0a2e92
Christina Cepeda <chriscepeda@gmail.com>
2023-02-03 12:14:49 +0100

    feb2

--- portfolio-update/src/component/Aboutme.jsx
+++ portfolio-update/src/component/Aboutme.jsx
@@ -6,56 +6,97 @@ import {Link} from 'react-router-dom'
 
 const About = () => {
   return (
-   
-    <div className='aboutme-container'>
 
-        
-        <div className='about-card'>
-        
-         <motion.div
-           initial={{y:20, opacity: 0,}}
-           animate={{y:0, opacity: 1,}}
-           transition={{duration:0.5, ease:'easeIn'}}
-           >
-          <h2>ABOUT ME</h2>
-           </motion.div>
+    <div className='about-hero'>
+    <div className='about-container'>
+     <motion.div
+     initial={{y:100, opacity: 0,}}
+     animate={{y:0, opacity: 1,}}
+     transition={{duration:0.5, ease:'easeIn'}}
+     
+     ><h1>ABOUT <br></br>CHRISTINA</h1>
+     </motion.div> 
+    </div>
 
-          <motion.div
-           initial={{y:20, opacity: 0,}}
-           animate={{y:0, opacity: 1,}}
-           transition={{delay: 2, duration:0.5, ease:'easeIn'}}
-           > <h3>CODE</h3>
-            <p> I'm currently doing my internship as a Frontend Developer 
-              at <a href="https://lemontree.se/" target="_blank" rel="noreferrer"> Lemontree</a>, a swedish IT-company creating digital transformation and 
-              increase quality in companies systems and services.<br></br></p>
-             
-             <p> In March 2023 I will receive my Diploma in Frontend Development from Hyper Island.
-              HI learning methods have been to integrate with the industry at once through projects in teams led by 
-              professionals. The diversity and the experimental way of learning has really made me developed great soft skills. 
-            </p>
-            </motion.div>
+    
+    <div className='about-wrapper'>
+          
+          <motion.div className='about-cardone'
+            initial={{y:100, opacity: 0,}}
+            animate={{y:0, opacity:1}}
+            transition={{delay:1.0, duration:0.8, ease:'easeIn'}}>
+          
+            <h1>01</h1>
+            <h4>FRONTEND
+            DEVELOPER</h4>
+            <p>#1 EXPERIENCE<br />
+            I'm currently doing my internship as a Frontend Developer 
+              at Lemontree, a Stockholm based IT-company with services in DevOps, TestOps, Development and Life Science. <br /> <br />
+              
+              #2 SKILLS <br />
+              Constantly buildning applications with both front-and backend techstack. Collaborating in teams practising 
+              an agile workflow through Kanban and Scrum methods. And implementing to the projects a design thinking through 
+              User-Centerd design (UX) and layout design (UI).<br /> <br />
+              
 
-            <motion.div
-              initial={{y:20, opacity: 0,}}
-              animate={{y:0, opacity: 1,}}
-              transition={{delay: 4, duration:0.5, ease:'easeIn'}}
-              > 
-              <h3>DESIGN</h3>
-              <p> I used to travel the world, seeing so much potential in the textile industry. So for severl years I had my own
-              business, collaborting with local tailors in India and Bali with pattern construction and designing cloth collections. 
-              This was a great opportunity and experience to explore materials, pattern design and sewing techniques.
-              Also managing retail, customer service and import and export; coordinating with shipping agents for exporting and importing goods.
-              <a href={'https://www.lemontree.se/'}>lemontree </a></p>
+              #3 QUALIFICATIONS <br />
+              In March 2023 I will graduate from Hyper Island and receive my Diploma in Frontend Development. Do browse my
+              portfolio to get a comprehensive look at my skills in Code, Design and Work experience.</p><br /> <br />
 
-            </motion.div>
+
+          </motion.div>
+
+          <motion.div className='about-cardtwo'
+            initial={{y:100, opacity: 0,}}
+            animate={{y:0, opacity:1}}
+            transition={{delay:1.0, duration:0.8, ease:'easeIn'}}>
+          
+            <h1>02</h1>
+            <h4>DESIGN</h4>
+            <p>#1 EXPERIENCE <br />
+               Collaborating with tailors and pattern makers in India/Rajastan and Indonesia/Bali, creating clothes collections.<br /> <br />
+               
+              #2 SKILLS <br />
+              Textile design: Sewing techniques / patternmaking / fabrics.<br />
+              Import and export: Coordinating with shipping agents for exporting and importing goods.<br />
+              Retail: Sales and customer service.<br /> <br />
+
+              #3 QUALIFICATIONS <br />
+              Design in Profession and Theory course / Borås Textilhögskola
+              Pattern making course / Malmö Folkhögskola </p><br /> <br />
+
+
+          </motion.div>
+
+          <motion.div className='about-cardone'
+            initial={{y:100, opacity: 0,}}
+            animate={{y:0, opacity:1}}
+            transition={{delay:1.0, duration:0.8, ease:'easeIn'}}>
+          
+            <h1>01</h1>
+            <h4>FRONTEND
+            DEVELOPER</h4>
+            <p>#1 EXPERIENCE<br />
+            I'm currently doing my internship as a Frontend Developer 
+              at Lemontree, a Stockholm based IT-company with services in DevOps, TestOps, Development and Life Science. <br /> <br />
+              
+              #2 SKILLS <br />
+              Constantly buildning applications with both front-and backend techstack. Collaborating in teams practising 
+              an agile workflow through Kanban and Scrum methods. And implementing to the projects a design thinking through 
+              User-Centerd design (UX) and layout design (UI).<br /> <br />
+              
+
+              #3 QUALIFICATIONS <br />
+              In March 2023 I will graduate from Hyper Island and receive my Diploma in Frontend Development. Do browse my
+              portfolio to get a comprehensive look at my skills in Code, Design and Work experience.</p><br /> <br />
+
+
+          </motion.div>
 
           
-          
+        
         </div>
-              
-              
-          
-            </div>
+      </div>
           
 
 
